10bf40d6e5a5c1f7d6f8aba310d2788c8d1c87ce
lebrancconvas <lebrancconvas@gmail.com>
2019-01-14 21:56:53 +0700

    :tada: Create Repository by Create-React-App .

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Input from './components/input'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Input />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/input.js
@@ -0,0 +1,13 @@
+import React , {Component} from 'react'
+
+export default class Input extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                   Search <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
